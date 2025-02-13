5e470a1991c5ed5dbb91e05a55971328bdbd9922
ZachMK25 <k.zach@wustl.edu>
2025-02-13 14:03:46 -0800

    very basic api endpoint, concepts of a plan for parsing description

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify
 
 import scrape_description
+from dotenv import dotenv_values
+import json
+import requests
+from bs4 import BeautifulSoup
+
+config = dotenv_values(".env")
+    
 
 app = Flask(__name__)
-    
 
 @app.route('/<video_id>', methods=['GET'])
 def get_item(video_id):
@@ -14,6 +20,38 @@ def get_item(video_id):
     else:
         return jsonify({"error": "Error Processing video"}), 404
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+    
+# @app.route('/ai/<video_id>', methods=["GET"])
+# def ai_get_item(video_id):
+#     print("AI approach",video_id)
+#     soup = BeautifulSoup(requests.get(youtube_url).content, features="html.parser")
+
+#     html_pattern = re.compile('(?<=shortDescription":").*(?=","isCrawlable)')
+
+#     description = html_pattern.findall(str(soup))[0].replace('\\n','\n')
+
+#     print(description)
+    
+#     url = config["OLLAMA_URL"]
+#     headers = {
+#         "Content-Type": "application/json"
+#     }
+    
+#     data = {
+#         "model":config["MODEL"],
+#         "prompt": config["PROMPT"] + description,
+#         "stream": False
+#     }
+    
+#     response = requests.post(url, headers=headers, data=json.dumps(data))
+    
+#     if response.status_code == 200:
+#         response_text = response.text
+#         data = json.loads(response_text)
+#         actual_response = data["response"]
+#         return jsonify(actual_response)
+#     else:
+#         print(response)
+#         return jsonify({"error": "Error Processing video"}), 404
--- scrape_description.py
+++ scrape_description.py
@@ -7,9 +7,14 @@ import sys
 
 def scrape_description(video_identifier):
 
+    # We can cut the whole cache thing once we refine the extraction, 
+    # I just done wanna get blocked by YouTube when I am making so many requests
+
+    FORCE_RENEW_CACHE = True
     # check if the cache file has been used before and has something in it.
     # using this for now to not blacklist us from youtube requests. need to figure out something more permanent in the future
     cache_exists = os.path.isfile("cache.txt") and os.stat("cache.txt").st_size != 0
+    
         
     youtube_url = "https://youtube.com/watch?v="+video_identifier
     
@@ -17,7 +22,7 @@ def scrape_description(video_identifier):
     
     
     # need to avoid rate limiting from youtube
-    if not cache_exists:
+    if not cache_exists or FORCE_RENEW_CACHE:
         # https://stackoverflow.com/questions/72354649/how-to-scrape-youtube-video-description-with-beautiful-soup
 
         soup = BeautifulSoup(requests.get(youtube_url).content, features="html.parser")
@@ -41,11 +46,20 @@ def scrape_description(video_identifier):
 
     # finding links within description
 
-    link_pattern = re.compile('https://[a-zA-Z\?\/\.]*')
+    link_pattern = re.compile('https://[a-zA-Z0-9\-\?\/\.\?]*')
 
     links = link_pattern.findall(description)
+    
+    shortened_link_pattern = re.compile('https?:\/\/(?:bit\.ly|goo\.gl|ow\.ly|tinyurl\.com)\/\S+')
+    shortened_links = shortened_link_pattern.findall(description)
+
+    # affiliate link pattern
+    affiliate_link_pattern = re.compile('https?:\/\/\S+\?(?:ref|affiliate_id|promo)=\S+')
+    affiliate_links = affiliate_link_pattern.findall(description)
 
     print("all links found in description:\n",links,"\n")
+    print("found the following shortened links\n",shortened_links,"\n")
+    print("found the following affiliate links\n",affiliate_links,"\n")
 
     # throw out links to socials, add more as needed
     # eventually want a more thorough way of validating whether a link would be to a sponsor
@@ -62,6 +76,10 @@ def scrape_description(video_identifier):
             
     print("sponsor links:\n",sponsor_links,"\n\n")
     
-    return sponsor_links
+    return {
+        "sponsor_links":sponsor_links,
+        "affiliate_links":affiliate_links,
+        "shortened_links":shortened_links
+    }
 
 sys.modules[__name__] = scrape_description
