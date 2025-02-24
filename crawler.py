a859da14ffcf536dbf262d97f7bf7e4b6754a5fa
victoriaprisco <p.victoria@wustl.edu>
2025-02-24 16:55:17 -0600

    fix crawler to get all video ids to scrape

--- crawler.py
+++ crawler.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
+import re
+
 # NEED TO FOLLOW YOUTUBE'S ROBOTS.TXT
 
 trending_url = "https://www.youtube.com/feed/trending"
@@ -11,8 +13,12 @@ soup = BeautifulSoup(html, features="html.parser")
 
 # <yt-formatted-string id="description-text" class="style-scope ytd-video-renderer">
 
-thumbnail_links = soup.find_all('a')
+# thumbnail_links = soup.find_all('a')
+# print(soup.html)
+html_pattern = re.compile('"videoId":"(.*?)"')
+all_video_ids = html_pattern.findall(str(soup))
 
-for link in thumbnail_links:
-    print(link)
-    print(link.get('href'))
+
+# for link in thumbnail_links:
+#     print(link)
+#     print(link.get('href'))
