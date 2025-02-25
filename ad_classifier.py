be49b77c155ecf9bd969a1f205ff21cbe9f19520
victoriaprisco <p.victoria@wustl.edu>
2025-02-24 18:23:31 -0600

    starting ad classificcation:

---
+++ ad_classifier.py
@@ -0,0 +1,13 @@
+#  possible link:
+    # social media
+    # resource related to the video
+        # require us to figure out what kind of video it is?
+    # affiliate link (includes code embedded in the link)
+    # link to sponsor (with no code)
+
+social_link = ""
+resource_link = ""
+affiliate_link = ""
+sponsor_link = ""
+
+#  lol i will start this later
